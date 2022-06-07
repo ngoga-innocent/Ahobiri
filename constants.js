812c6ec7c7492dceabcc00cef54d1defa1b2535a
Ngoga Innocent Patrick <Ngogainnocent1@gmail.com>
2022-06-07 11:38:24 +0200

    First push

---
+++ constants.js
@@ -0,0 +1,6 @@
+const nosService = [
+    { id: 1, service: 'Fast Food', image: require("./images/photo2.jpg") },
+    { id: 2, service: 'Shopping', image: require('./images/supermarket2.jpg') },
+    {id:3,service:'Door to Door Delivery',image:require('./images/Delivery3.jpg')}
+]
+export default nosService
